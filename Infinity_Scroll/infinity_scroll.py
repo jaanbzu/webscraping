aa01ffbd3bf362e7102cd6afd71ae70d7420d7b0
Muhammad Ramzan <88158892+ramzan30@users.noreply.github.com>
2021-08-19 21:59:30 +0500

    Update infinity_scroll.py

--- Infinity_Scroll/infinity_scroll.py
+++ Infinity_Scroll/infinity_scroll.py
@@ -7,7 +7,7 @@ from selenium.webdriver.common.keys import Keys
 #open browser
 driver = webdriver.Opera()
 #open website with infinity scroll
-driver.get('Enter web Address')
+driver.get('web Address')
 # how many time yuo want to scroll down. writer range_value
 for _ in range(11):
     driver.find_element_by_tag_name('body').send_keys(Keys.END)
